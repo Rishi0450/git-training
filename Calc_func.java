2d32388f1a29d7605c4efe17d5314e70f072fc88
Rishi kumar <rishi.nageswaran@a-cti.com>
2016-04-09 13:37:46 +0530

    loop function was added
    into calc_func by Rishi

--- Calc_func.java
+++ Calc_func.java
@@ -20,4 +20,49 @@ public class Calc_func
         else
             System.out.println("Denominator cannot be zero");
     }
+    static void loops(String str,int b,int sp,int p,int k)
+    {
+        	try{
+		
+			if(k==p/2+1)
+			{
+			System.out.println(String.format("%1$" +((p/2)+1)+ "s",str));
+			calx(str,b,sp,p,k+1);
+			}
+		
+		if(k<p/2+1)
+		{
+			
+			System.out.print(String.format("%1$"+b+"s",str));
+			System.out.println(String.format("%1$"+sp+"s",str));
+		b++;
+		sp=sp-2;
+		calx(str,b,sp,p,k+1);
+		}
+		
+	 if(k>p/2+1 && k!=p+1)
+		{
+		// if(b != 1){
+		//System.out.println(" b::::::: "+b);
+			b--;
+		 
+			sp=sp+2;
+			 System.out.print(String.format("%1$"+b+"s",str));
+			System.out.println(String.format("%1$"+sp+"s",str));
+		calx(str,b,sp,p,k+1);
+		 //}
+		}
+		
+	 if (k==p+1)
+ 		return 0;
+	}
+	catch(Exception e)
+	{
+			System.err.println(e);
+			e.printStackTrace();
+	}
+ 	return 0;
+	
+
+    }
 }
